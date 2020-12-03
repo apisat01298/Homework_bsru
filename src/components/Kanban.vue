2606c5291c9ed867d4dbb5e0c5353651f2a163d1
minetor <apisat01298@gmail.com>
2020-12-03 17:48:55 +0700

    kongPkao

--- src/components/Kanban.vue
+++ src/components/Kanban.vue
@@ -2,13 +2,16 @@
   <div class="kanban">
     <div
       class="column"
-      :style="{ backgroundColor: column.colors }"
+      :style="{ backgroundColor: column.color }"
       v-for="(column, index) in data"
       :key="index"
     >
-      <div class="column-header">{{ column.name }}</div>
+      <div class="column-header">
+        {{ column.name }}
+      </div>
+
       <div class="column-body">
-        <div v-for="(task, task_index) in column.task" :key="task_index">
+        <div v-for="(task, task_index) in column.tasks" :key="task_index">
           <div
             class="task"
             :draggable="true"
@@ -21,8 +24,10 @@
             @dragenter.prevent="drop_zone_enter"
             @dragleave.prevent="drop_zone_leave"
             @dragover.prevent
+            @drop="drop_item(index,task_index)"
           ></div>
         </div>
+
         <div class="create-task" @click="create_task(index)">Create Task</div>
       </div>
     </div>
@@ -41,6 +46,7 @@ export default {
   props: {
     data: Array,
     create_task_submit: Function,
+    move_item_task:Function
   },
   methods: {
     create_task(index_column) {
@@ -51,11 +57,10 @@ export default {
       this.create_task_submit(this.current_column_index, {
         task_name: this.task_name,
       });
-      alert(this.current_column_index + " " + this.task_name);
     },
     start_move(task_index, column_index) {
       this.current_column_index = column_index;
-      this.create_task_index = task_index;
+      this.current_task_index = task_index;
     },
     drop_zone_enter(event) {
       event.target.style.height = "100px";
@@ -67,12 +72,20 @@ export default {
       event.target.style.borderStyle = "none";
       event.target.style.transition = "height 0.5s";
     },
+    drop_item(column_index,task_index){
+        this.move_item_task(
+            this.current_column_index,
+            this.current_task_index,
+            column_index,
+            task_index
+        )
+    }
   },
   data() {
     return {
       task_name: "",
       current_column_index: "",
-      create_task_index: "",
+      current_task_index: "",
     };
   },
 };
@@ -80,9 +93,9 @@ export default {
 
 <style>
 .kanban {
-  background-color: pink;
   width: 100%;
   height: 100%;
+  background-color: bisque;
 }
 .column {
   height: 600px;
@@ -90,10 +103,10 @@ export default {
   border-radius: 10px;
   display: inline-block;
   margin: 25px;
-  -webkit-box-shadow: 10px 24px 35px 8px rgba(31, 15, 31, 1);
-  -moz-box-shadow: 10px 24px 35px 8px rgba(31, 15, 31, 1);
-  box-shadow: 10px 24px 35px 8px rgba(31, 15, 31, 1);
   padding: 10px;
+  -webkit-box-shadow: 0px 4px 15px 0px rgba(0, 0, 0, 0.75);
+  -moz-box-shadow: 0px 4px 15px 0px rgba(0, 0, 0, 0.75);
+  box-shadow: 0px 4px 15px 0px rgba(0, 0, 0, 0.75);
 }
 .column-header {
   display: flex;
@@ -104,32 +117,31 @@ export default {
   font-weight: bold;
 }
 .column-body {
-  height: calc(100% - 50px);
+  height: calc(100% - 60px);
   border-radius: 10px;
   padding: 5px;
-  background-color: rgba(255, 255, 255, 0.575);
+  background-color: #ffffff7c;
 }
 .create-task {
   width: 100%;
   height: auto;
   padding: 10px;
   border-radius: 5px;
-  font-size: 18px;
   cursor: pointer;
 }
 .create-task:hover {
-  background-color: white;
+  background-color: rgb(158, 155, 155);
 }
 .input-task-name {
   width: 100%;
 }
 .task {
+  /* position: relative; */
   width: auto;
-  position: relative;
   height: 100px;
-  border-radius: 3px;
+  border-radius: 2px;
   margin: 10px;
-  background-color: white;
+  background-color: bisque;
 }
 .drop_zone {
   height: 10px;
